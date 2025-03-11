addeab65bfe51b7680aadc6428ae5151a6839305
nimo731 <patience.karanja@student.moringaschool.com>
2025-03-11 09:18:51 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let messages = []; // Corrected variable name
+
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return messages; // Return the correct variable
+}
+
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"));
+
+function countDown(number) {  // Updated function name to match the test
+    while (number >= 0) {
+        console.log(number);
+        number--;  
+    }
+}
+
+countDown(10);
